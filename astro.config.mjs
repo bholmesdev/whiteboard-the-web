600361e64e3e5d4bb21c02de8caa42399d888e44
bholmesdev <bholmesdev@gmail.com>
2022-06-16 15:23:29 -0400

    initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,8 @@
+import { defineConfig } from 'astro/config';
+
+import netlify from "@astrojs/netlify";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [netlify()]
+});
