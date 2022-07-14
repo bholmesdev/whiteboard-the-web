a87beecaf8c401cf00a2acc1fded4bd5f87f7c25
bholmesdev <bholmesdev@gmail.com>
2022-07-14 15:38:00 -0400

    feat: random tweet colors

--- src/utils.ts
+++ src/utils.ts
@@ -10,3 +10,10 @@ export function toWhiteboardTheWebTweets(tweets: Tweet[]) {
     return hasHashtag && !isGoingLiveNotif;
   })
 }
+
+const SANCTIONED_COLORS = ['#FF007A', '#6100FF', '#0085FF', '#FF1F00', '#10BB4A']
+
+export function toRandomNotUglyColor() {
+  const randomIdx = Math.floor(Math.random() * SANCTIONED_COLORS.length)
+  return SANCTIONED_COLORS[randomIdx];
+}
