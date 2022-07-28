2b8bf426f0de7bd7acb8fc9e994466d87cc5bcf4
bholmesdev <bholmesdev@gmail.com>
2022-07-28 16:07:00 -0400

    chore: remove solid-js

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,4 @@
 import { defineConfig } from 'astro/config';
-import netlify from "@astrojs/netlify";
-import solid from "@astrojs/solid-js";
 
 // https://astro.build/config
-export default defineConfig({
-  integrations: [netlify(), solid()]
-});
+export default defineConfig({});
