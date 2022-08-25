b3415071de2f932ab5fc8765b64b9f4ccd11abf8
bholmesdev <bholmesdev@gmail.com>
2022-08-25 15:00:44 -0400

    chore: add vercel serverless

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from 'astro/config';
-
 import solid from "@astrojs/solid-js";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [solid()],
   output: 'server',
+  adapter: vercel()
 });
