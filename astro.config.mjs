47bf9814e6a8217948eb96db26afac48c975f5ab
bholmesdev <bholmesdev@gmail.com>
2022-08-25 15:14:40 -0400

    funsies: switch to preact

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
 import { defineConfig } from 'astro/config';
-import solid from "@astrojs/solid-js";
-
 import vercel from "@astrojs/vercel/serverless";
+
+import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [solid()],
+  integrations: [preact()],
   output: 'server',
   adapter: vercel()
 });
