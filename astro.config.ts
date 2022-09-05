d1cf6e72a72bf999e99b67a4f71e4f306ebab941
bholmesdev <bholmesdev@gmail.com>
2022-09-05 18:42:43 -0400

    chore: remove unused import

--- astro.config.ts
+++ astro.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from "astro/config";
 import solid from "@astrojs/solid-js";
 import vercel from "@astrojs/vercel/serverless";
 import { getEditionNumber } from "./src/utils";
-import { visit } from "unist-util-visit";
 
 function remarkDefaultLayout() {
   return (tree, { basename, data }) => {
