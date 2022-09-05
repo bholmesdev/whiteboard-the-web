c27673781d3d7f7e458b123f76d4fd5cf918e284
bholmesdev <bholmesdev@gmail.com>
2022-09-05 17:34:06 -0400

    chore: remove dead code

--- src/_types.ts
+++ src/_types.ts
@@ -1,38 +1,8 @@
-export type TweetResponse = {
-  data: Tweet[];
-  includes: {
-    media: Media[];
-  };
-  meta: {};
+import type { MarkdownInstance } from "astro";
+
+export type VideoFrontmatter = {
+  twitter: string;
+  youtube: string;
 };
 
-export type Tweet = {
-  id: string;
-  text: string;
-  entities?: {
-    hashtags?: {
-      start: number;
-      end: number;
-      tag: string;
-    }[];
-  };
-  attachments?: {
-    media_keys: string[];
-  };
-};
-
-export type Variant = {
-  content_type: "video/mp4" | "application/x-mpegURL";
-  bit_rate?: number;
-  url: string;
-};
-
-export type Media = {
-  media_key: string;
-} & (
-  | { type: "photo" }
-  | {
-      type: "video";
-      variants: Variant[];
-    }
-);
+export type VideoDocument = MarkdownInstance<VideoFrontmatter>;
