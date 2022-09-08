54de96a74edb2b5a1b642bafde8d1f7af2fbdaf3
bholmesdev <bholmesdev@gmail.com>
2022-09-08 16:01:41 -0400

    feat: change background color on scroll 🚀

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -7,9 +7,15 @@ type Props = {
   src: string;
   type: "youtube" | "twitter";
   title?: string;
+  colorIntersectionObserver?: boolean;
 };
 
-export default function Video({ src, type, title }: Props) {
+export default function Video({
+  src,
+  type,
+  title,
+  colorIntersectionObserver = true,
+}: Props) {
   let containerRef: HTMLElement | null | undefined;
   const [isPlaying, setIsPlaying] = createSignal(false);
   const isOffscreenObserver = new IntersectionObserver((changes) => {
@@ -17,6 +23,20 @@ export default function Video({ src, type, title }: Props) {
       if (!change.isIntersecting) handlePlaybackToggle(false);
     }
   });
+  const isTopOfPageObserver = new IntersectionObserver(
+    (changes) => {
+      for (const change of changes) {
+        if (change.isIntersecting && containerRef) {
+          const colorHs =
+            getComputedStyle(containerRef).getPropertyValue("--color-hs");
+          (
+            document.querySelector("#color-backdrop") as HTMLElement
+          ).style.setProperty("--color-hs", colorHs);
+        }
+      }
+    },
+    { rootMargin: "0px 0px -70% 0px", threshold: 0 }
+  );
 
   function handlePlaybackToggle(isPlaying: boolean) {
     setIsPlaying(isPlaying);
@@ -34,6 +54,9 @@ export default function Video({ src, type, title }: Props) {
             class={c.player}
             ref={(el) => {
               isOffscreenObserver.observe(el);
+              if (colorIntersectionObserver) {
+                isTopOfPageObserver.observe(el);
+              }
               containerRef = el.closest("article");
             }}
           >
