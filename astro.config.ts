5d02f67b76a2ef20fef5cd0c3dc395f4de20d6d6
bholmesdev <bholmesdev@gmail.com>
2023-06-12 09:43:19 -0400

    feat: move redirects to experimental redirects

--- astro.config.ts
+++ astro.config.ts
@@ -8,4 +8,13 @@ export default defineConfig({
   integrations: [solid()],
   output: "server",
   adapter: vercel(),
+  redirects: {
+    "/chat": "https://discord.gg/NkCkXtJ9",
+    "/newsletter": "https://buttondown.email/bholmesdev",
+    "/rainy": "https://github.com/bholmesdev/astro-client-when-rainy-in-ny",
+    "/vite-week": "https://twitter.com/BHolmesDev/status/1541423974202150913",
+  },
+  experimental: {
+    redirects: true,
+  },
 });
