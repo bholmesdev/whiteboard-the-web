0efe79417ddc5785cbe191cbddd8b281fafcdae4
bholmesdev <bholmesdev@gmail.com>
2023-06-22 15:57:12 -0400

    fix: update to non-expiring discord invite

--- astro.config.ts
+++ astro.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
   output: "server",
   adapter: vercel(),
   redirects: {
-    "/chat": "https://discord.gg/NkCkXtJ9",
+    "/chat": "https://discord.gg/jBqmtCBVN4",
     "/newsletter": "https://buttondown.email/bholmesdev",
     "/rainy": "https://github.com/bholmesdev/astro-client-when-rainy-in-ny",
     "/vite-week": "https://twitter.com/BHolmesDev/status/1541423974202150913",
