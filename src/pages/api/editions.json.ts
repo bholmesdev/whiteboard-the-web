a2583aee45eca60a4012cff63253229514f630b8
bholmesdev <bholmesdev@gmail.com>
2023-07-04 13:11:52 -0400

    fix: sort editions.json

--- src/pages/api/editions.json.ts
+++ src/pages/api/editions.json.ts
@@ -1,18 +1,23 @@
 import { getCollection } from "astro:content";
+import { getEditionInfo } from "~utils";
 
 export const prerender = true;
 
 export async function get() {
   const editions = await getCollection("editions");
-  const editionsWithHeadings = await Promise.all(
-    editions.map(async (edition) => {
-      const { headings } = await edition.render();
-      return {
-        ...edition,
-        headings,
-      };
-    })
-  );
+  const editionsWithHeadings = (
+    await Promise.all(
+      editions.map(async (edition) => {
+        const { headings } = await edition.render();
+        const number = getEditionInfo(edition.id)?.num ?? 0;
+        return {
+          ...edition,
+          headings,
+          number,
+        };
+      })
+    )
+  ).sort((a, b) => b.number - a.number);
 
   return new Response(JSON.stringify(editionsWithHeadings));
 }
