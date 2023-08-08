2087c355e0e1b754bb7a3de7e779edceef52f2e5
bholmesdev <bholmesdev@gmail.com>
2023-08-08 10:10:35 -0400

    chore: linear and button redirects

--- astro.config.ts
+++ astro.config.ts
@@ -11,6 +11,8 @@ export default defineConfig({
     "/rainy": "https://github.com/bholmesdev/astro-client-when-rainy-in-ny",
     "/vite-week": "https://twitter.com/BHolmesDev/status/1541423974202150913",
     "/transitions": "https://twitter.com/BHolmesDev/status/1682206832423579649",
+    "/linear": "https://linear-easing-generator.netlify.app",
+    "/button": "https://codepen.io/bholmesdev-the-reactor/full/xxQBvqR",
   },
   experimental: {
     viewTransitions: true,
