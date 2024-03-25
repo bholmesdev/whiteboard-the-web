8afeb3c161d893efe2cce68bd0a401cb8e495f42
bholmesdev <bholmesdev@gmail.com>
2024-03-24 23:09:35 -0400

    feat: prevent named exports from actions files

--- astro.config.ts
+++ astro.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "astro/config";
 import db from "@astrojs/db";
 import vercel from "@astrojs/vercel/serverless";
 import simpleStackFrame from "simple-stack-frame";
+import { parse as esModuleParse } from "es-module-lexer";
 
 import react from "@astrojs/react";
 
@@ -13,17 +14,34 @@ export default defineConfig({
   integrations: [db(), simpleStackFrame(), react()],
   vite: {
     esbuild: {
-      keepNames: true
+      keepNames: true,
     },
-    plugins: [{
-      name: "action-resolver",
-      load(id, options) {
-        if (id.includes("/src/pages/")) {
-          if (options?.ssr) return;
-          // TODO: use pagesDir and id resolve
-          const pagesDir = new URL("src/pages/", import.meta.url).pathname;
-          const path = id.replace(pagesDir, "").replace(/\.(ts|js)$/, "");
-          return `
+    plugins: [
+      {
+        name: "action-resolver",
+        transform(code, id) {
+          if (id.includes("/src/pages/")) {
+            const [_, exports] = esModuleParse(code);
+            if (!exports.some((exp) => exp.n === "actions")) return;
+
+            const hasNonActionExports = exports.some(
+              (exp) => exp.n !== "actions" && exp.n !== "prerender"
+            );
+            if (hasNonActionExports) {
+              const formattedId = id.slice(id.indexOf("/src/pages/") + 11);
+              throw new Error(
+                `API route **${formattedId}** cannot have other exports when using 'actions'. Move other exports to a separate file.`
+              );
+            }
+          }
+        },
+        load(id, options) {
+          if (id.includes("/src/pages/")) {
+            if (options?.ssr) return;
+            // TODO: use pagesDir and id resolve
+            const pagesDir = new URL("src/pages/", import.meta.url).pathname;
+            const path = id.replace(pagesDir, "").replace(/\.(ts|js)$/, "");
+            return `
             const actionProxy = new Proxy({}, {
               get: (_, action) => {
                 return async (formData) => {
@@ -41,8 +59,9 @@ export default defineConfig({
 
             export const actions = actionProxy;
           `;
-        }
-      }
-    }]
-  }
+          }
+        },
+      },
+    ],
+  },
 });
